7dc264871ffc92283db8162476cd74525f9a8026
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-04-27 02:15:02 +0800

    included a hardcoded connectwise api call

--- src/events/ticketChecker.js
+++ src/events/ticketChecker.js
@@ -1,18 +1,56 @@
 import api from "../configs/apiKey.json";
 
-async function callAPI(arg, props) {
+async function updateCWTicketStatus(json) {
+  // json.status.id = 499;
+
+  const modifiedJson = {
+    ...json,
+    status: {
+      ...json.status,
+      id: 449,
+    },
+  };
+
+  const response = await fetch(
+    "https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/100",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        clientId: "dda341d3-f8bc-4fc1-9b99-e6721e35bae7",
+        Authorization:
+          "Basic YW5jaG9yc2l4X2NzMStMVTh4Z3dmRkxKaEZkUFVEOmdaTlN0N1M5Vm04MW9mRjE=",
+      },
+      body: JSON.stringify(modifiedJson),
+    }
+  );
+
+  const ticket = await response.json();
+
+  if (response.ok) {
+    console.log(JSON.stringify(ticket));
+  }
+
+  if (!response.ok) {
+    console.log("Fail");
+  }
+}
+
+async function getCWTicket(arg) {
   console.log(`Status: ${arg.changes.workflowStatus.to.attributes.name}`);
   if (arg.changes.workflowStatus.to.attributes.name === "Released") {
     console.log("Status == Released. Api called.");
 
     try {
       const response = await fetch(
-        "https://aha-endpoints-migration.srilan-catalinio.workers.dev/",
+        "https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/100?conditions=status/name='New'",
         {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            API_KEY: api.apiKey,
+            clientId: "dda341d3-f8bc-4fc1-9b99-e6721e35bae7",
+            Authorization:
+              "Basic YW5jaG9yc2l4X2NzMStMVTh4Z3dmRkxKaEZkUFVEOmdaTlN0N1M5Vm04MW9mRjE=",
           },
         }
       );
@@ -20,8 +58,7 @@ async function callAPI(arg, props) {
       const json = await response.json();
 
       if (response.ok) {
-        console.log(`Response: ${JSON.stringify(json.status)}`);
-        console.log(`Props: ${props}`);
+        updateCWTicketStatus(json);
       }
 
       if (!response.ok) {
@@ -35,8 +72,8 @@ async function callAPI(arg, props) {
   }
 }
 
-export default callAPI;
+export default getCWTicket;
 
-aha.on({ event: "aha.update.Feature.workflowStatus" }, (arg, props) => {
-  callAPI(arg, props);
+aha.on({ event: "aha.update.Feature.workflowStatus" }, (arg) => {
+  getCWTicket(arg);
 });
