32d38ed3e789d251f7e1d856a44789d4a3384050
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-04-27 04:33:32 +0800

    done

--- src/events/ticketChecker.js
+++ src/events/ticketChecker.js
@@ -1,4 +1,4 @@
-async function updateCWTicketStatus(json, ticketId) {
+async function updateCWTicketStatus(json, CWId) {
   const modifiedJson = {
     ...json,
     status: {
@@ -8,7 +8,7 @@ async function updateCWTicketStatus(json, ticketId) {
   };
 
   const response = await fetch(
-    `https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/${ticketId}`,
+    `https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/${CWId}`,
     {
       method: "PUT",
       headers: {
@@ -25,7 +25,9 @@ async function updateCWTicketStatus(json, ticketId) {
 
   if (response.ok) {
     console.log(
-      `Status was successfully updated to ${JSON.stringify(ticket.status.name)}`
+      `${CWId}'s status was successfully updated to ${JSON.stringify(
+        ticket.status.name
+      )}`
     );
   }
 
@@ -34,45 +36,65 @@ async function updateCWTicketStatus(json, ticketId) {
   }
 }
 
-async function getCWTicket(arg, ticketId) {
-  console.log(`Status: ${arg.changes.workflowStatus.to.attributes.name}`);
-  if (arg.changes.workflowStatus.to.attributes.name === "Released") {
-    console.log("Status == Released. Api called.");
-
-    try {
-      const response = await fetch(
-        `https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/${ticketId}?conditions=status/name='New'`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            clientId: "dda341d3-f8bc-4fc1-9b99-e6721e35bae7",
-            Authorization:
-              "Basic YW5jaG9yc2l4X2NzMStMVTh4Z3dmRkxKaEZkUFVEOmdaTlN0N1M5Vm04MW9mRjE=",
-          },
-        }
-      );
-
-      const json = await response.json();
-
-      if (response.ok) {
-        updateCWTicketStatus(json, ticketId);
+async function getCWTicket(CWId) {
+  try {
+    const response = await fetch(
+      `https://sandbox-na.myconnectwise.net/v4_6_release/apis/3.0/service/tickets/${CWId}?conditions=status/name='New'`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          clientId: "dda341d3-f8bc-4fc1-9b99-e6721e35bae7",
+          Authorization:
+            "Basic YW5jaG9yc2l4X2NzMStMVTh4Z3dmRkxKaEZkUFVEOmdaTlN0N1M5Vm04MW9mRjE=",
+        },
       }
+    );
 
-      if (!response.ok) {
-        console.log("API call failed.");
-      }
-    } catch (error) {
-      console.log(error);
+    const json = await response.json();
+
+    if (response.ok) {
+      updateCWTicketStatus(json, CWId);
     }
+
+    if (!response.ok) {
+      console.log("API call failed.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function ahaToCWTicket(ahaId) {
+  const response = await fetch(
+    `https://trajector1.aha.io/api/v1/features/${ahaId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer c91c04bc38114c2cbc727a42ec1bee8ef6b05361b363ef685bb4e46945bb76fd`,
+      },
+    }
+  );
+
+  if (response.ok) {
+    const json = await response.json();
+    const CWId = JSON.parse(json.feature.custom_fields[1].body);
+    getCWTicket(CWId);
   } else {
-    console.log("Status != Released. Api not called.");
+    console.error("API call failed:", await response.text());
   }
 }
 
 export default getCWTicket;
 
 aha.on({ event: "aha.update.Feature.workflowStatus" }, (arg) => {
-  const ticketId = 100;
-  getCWTicket(arg, ticketId);
+  console.log(`Status: ${arg.changes.workflowStatus.to.attributes.name}`);
+  if (arg.changes.workflowStatus.to.attributes.name === "Released") {
+    console.log("Status == Released. Api called.");
+    const ahaId = arg.record.attributes.id;
+    ahaToCWTicket(ahaId);
+  } else {
+    console.log("Status != Released. Api not called.");
+  }
 });
